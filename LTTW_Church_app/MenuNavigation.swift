8159819268c369e2750921bb9899cbc3b9061261
artsiomv <artsiomv@uw.edu>
2019-02-03 13:16:15 -0800

    transitioning menu items to buttons

---
+++ LTTW_Church_app/MenuNavigation.swift
@@ -0,0 +1,34 @@
+//
+//  MenuNavigation.swift
+//  LTTW_Church_app
+//
+//  Created by Artiom on 2/3/19.
+//  Copyright © 2019 Artiom. All rights reserved.
+//
+
+import UIKit
+
+class MenuNavigation: UIViewController {
+
+    @IBAction func HomeButton(_ sender: Any) {
+        self.
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
