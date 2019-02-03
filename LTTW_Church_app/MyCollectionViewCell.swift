9dc2d110b0fbd2e4802161e0302dd72b22bf7a6f
artsiomv <artsiomv@uw.edu>
2019-02-02 22:27:20 -0800

    Navigation Bar remastered, now it is a real "Navigation Bar" Menu and Back buttons work

---
+++ LTTW_Church_app/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  LTTW_Church_app
+//
+//  Created by Artiom on 1/30/19.
+//  Copyright © 2019 Artiom. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+     @IBOutlet weak var myLabel: UILabel!
+}
